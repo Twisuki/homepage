61d0bd3591da456bef5e905b022c335aba16617c
Twisuki <Twisuki@outlook.com>
2026-05-04 19:33:43 +0800

    :sparkles: 日期卡片

--- src/app/[locale]/(pages)/state/components/date.tsx
+++ src/app/[locale]/(pages)/state/components/date.tsx
@@ -1,9 +1,91 @@
+import type { Dayjs } from "dayjs"
+import dayjs from "dayjs"
+import { useTranslations } from "next-intl"
 import Base from "@/app/[locale]/(pages)/state/components/base"
+import { dateList } from "@/data/date"
+import { cn } from "@/lib/cn"
+
+function Calendar() {
+  const days: Dayjs[] = []
+
+  for (let i = dayjs().startOf("month").day(); i > 0; i--) {
+    days.push(dayjs().startOf("month").subtract(i, "day"))
+  }
+
+  for (let i = 0; i < dayjs().daysInMonth(); i++) {
+    days.push(dayjs().startOf("month").add(i, "day"))
+  }
+
+  const remaining = 42 - days.length
+  for (let i = 0; i < remaining; i++) {
+    days.push(dayjs().endOf("month").add(i + 1, "day"))
+  }
+
+  const WEEKDAYS = ["日", "一", "二", "三", "四", "五", "六"]
+
+  return (
+    <div className="w-full grid grid-cols-7 grid-rows-7">
+      {WEEKDAYS.map(d => (
+        <div
+          key={d}
+          className="flex items-center justify-center"
+        >
+          {d}
+        </div>
+      ))}
+      {days.map(day => (
+        <div
+          key={day.format("YYYY-MM-DD")}
+          className={cn(
+            "flex items-center justify-center hover:bg-white/20 rounded-lg",
+            { "bg-blue-500 text-white rounded-lg hover:bg-blue-600": day.isSame(dayjs(), "day") },
+            { "text-white/30": !day.isSame(dayjs(), "month") },
+          )}
+        >
+          {day.date()}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+function Message() {
+  const t = useTranslations("StatePage.date")
+
+  const message = dateList.find(d => d.date === dayjs().format("MM-DD"))?.message
+
+  if (message) {
+    return (
+      <>
+        {message}
+      </>
+    )
+  }
+
+  if (dayjs().day() === 0 || dayjs().day() === 6) {
+    return (
+      <>
+        {t("weekend")}
+      </>
+    )
+  }
+
+  return (
+    <>
+      {t("workday")}
+    </>
+  )
+}
 
 export default function Date() {
   return (
-    <Base x={2} y={2}>
-      Date
+    <Base
+      x={2}
+      y={2}
+      className="flex flex-col justify-center items-center gap-2 text-sm"
+    >
+      <Calendar />
+      <Message />
     </Base>
   )
 }
---
+++ src/data/date.ts
@@ -0,0 +1,10 @@
+export interface Date {
+  date: string
+  message: string
+}
+
+export const dateList: Date[] = [
+  { date: "05-04", message: "致敬传奇LV5!" },
+  { date: "05-21", message: "非常神奇的一天" },
+  { date: "05-31", message: "好像有人过生日" },
+]
